01921f239cf575fe3b8ce7433f8e18dd24451c5d
rahulanku1902 <rahulanku1902@gmail.com>
2025-06-04 19:31:01 +0530

    add blog in naviagtion

--- src/app/(frontend)/components/global/Menu/HeaderMenu.jsx
+++ src/app/(frontend)/components/global/Menu/HeaderMenu.jsx
@@ -7,7 +7,10 @@ import React from "react";
 const HeaderMenu = () => {
   const pathname = usePathname();
   return (
-    <ul className="navbar-nav ms-5 ms-auto  mb-lg-0 ">
+    <ul
+      className="navbar-nav ms-5 ms-auto mb-lg-0"
+      style={{ display: "flex", flexDirection: "row", gap: "1rem" }}
+    >
       <li className="nav-item">
         <Link
           href="/product"
@@ -16,6 +19,16 @@ const HeaderMenu = () => {
           }`}
         >
           Product
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          href="/blog"
+          className={`nav-link f-c-s ff-j f-20 f-w-400 px-md-3 px-2 py-md-2 py-1 customLinks ${
+            pathname === "/blog" ? "active" : ""
+          }`}
+        >
+          Blog
         </Link>
       </li>
     </ul>
