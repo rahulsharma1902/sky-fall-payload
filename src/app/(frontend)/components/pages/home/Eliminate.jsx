db5ebfdcfcaca615a50201a697690bb7bcc8eb01
rahulanku1902 <rahulanku1902@gmail.com>
2025-06-09 17:32:41 +0530

    remove p error

--- src/app/(frontend)/components/pages/home/Eliminate.jsx
+++ src/app/(frontend)/components/pages/home/Eliminate.jsx
@@ -17,12 +17,12 @@ const Eliminate = () => {
             </h2>
 
             <p className="technology-content">
-            Skyfall deploys thousands of World Model powered agents to solve your most complex IT 
-            tickets - fast. From complex support to infrastructure fixes, our agents execute with
-             automated precision, zero human error and no overhead. Stop firefighting, and focus on
-              creating strategic business value for the organization.
-
-            <p className="f-20 f-c-s ff-i f-w-400 d-none d-md-block">
+            Skyfall deploys thousands of World Model-powered agents to resolve your most complex
+            IT tickets—fast. From infrastructure issues to escalated support cases, our agents 
+            execute with automated precision, zero human error, and no operational overhead.
+            Stop firefighting. Start delivering strategic value.
+            </p>
+            {/* <p className="f-20 f-c-s ff-i f-w-400 d-none d-md-block">
             Skyfall deploys thousands of World Model-powered agents to resolve your most complex
             <br />
             IT tickets—fast. From infrastructure issues to escalated support cases, our agents
@@ -31,7 +31,7 @@ const Eliminate = () => {
              <br />
              Stop firefighting. Start delivering strategic value.
             
-            </p>
+            </p> */}
           </div>
         </div>
         <div className="row py-md-3 py-lg-5">
