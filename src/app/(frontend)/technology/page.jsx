daf71fcf1d1f7428922b977efbc2318e4479c9fb
rahulanku1902 <rahulanku1902@gmail.com>
2025-06-11 12:19:06 +0530

    about us section added to technology page

--- src/app/(frontend)/technology/page.jsx
+++ src/app/(frontend)/technology/page.jsx
@@ -3,13 +3,15 @@ import WorldIT from "../components/pages/home/WorldIT";
 import WorldModel from "../components/pages/technology/WorldModel";
 import TechBg from "../components/pages/technology/TechBg";
 import TechDescription from "../components/pages/technology/TechDescription";
+import Aboutus from '../components/pages/home/Aboutus'
 
 export default function Technology() {
   return (
-    <section>
+    <section>a
       <TechBg />
       <WorldModel />
       <TechDescription />
+      <Aboutus />
     </section>
   );
 }
